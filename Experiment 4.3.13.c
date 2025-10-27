fe190be87acab1541de41f124fd5b78123d62391
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 15:03:30 +0530

    Create Experiment 4.3.13.c

---
+++ Experiment 4.3.13.c
@@ -0,0 +1,22 @@
+/* 4.3 Patterns: 13*/
+#include<stdio.h>
+int main()
+{
+printf("Mahek Tilara\nSAP ID: 590025452\nCourse: BSc. CS. \nBatch: 1");
+printf("\n-----------------\n");
+printf("The pattern is:\n");
+int i, j, k;
+for(i=1; i<6; i++)
+{
+for(k=1; k<=i; k++)
+{
+printf(" ");
+}
+{
+for(j=5; j>=i; j--)
+printf("%d", j);
+}
+printf("\n");
+}
+return 0;
+}
