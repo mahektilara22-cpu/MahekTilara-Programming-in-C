6005b37a0d19f963b7401c47969d38d09303cd74
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 14:28:09 +0530

    Create Experiment 1.4.c

---
+++ Experiment 1.4.c
@@ -0,0 +1,21 @@
+/*Experiment 1: installation, Environment setup
+and starting with C language
+4. Write a C program to perform any four arithmetic operation on
+two variable. */
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+printf("Name- Mahek Jayantilal Tilara\n SAP
+ID – 590025452 \n Course-BSC CS\n BatchB1 \n");
+float a,b;
+printf("Enter the value of a: ");
+scanf("%f", &a);
+printf("Enter the value of b: ");
+scanf("%f",&b);
+printf("\nAddition: %2f\n", a+b);
+printf("Subtraction: %2f\n", a-b);
+printf("Multiplication: %2f\n", a*b);
+printf("Division: %2f\n", a/b);
+return 0;
+}
