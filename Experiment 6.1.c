3d09909bc845c70a63661504cf5402ca4d494937
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 15:12:21 +0530

    Create Experiment 6.1.c

---
+++ Experiment 6.1.c
@@ -0,0 +1,36 @@
+/*Experiment 6: Array
+6.1. WAP to read a list of integers and store it in a single dimensional array. Write a C
+program to print the second largest integer in a list of integers.*/
+#include<stdio.h>
+int main()
+{
+printf(“Mahek Tilara\nSAP ID: 590025452\nCourse- BSc CS\nBatch-1");
+printf("\n---------------------------------------------------------------------------------\n");
+int n, i;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+int arr[n];
+printf("Enter %d elements:\n", n);
+for ( i = 0; i < n; i++)
+{
+scanf("%d", &arr[i]);
+}
+int largest = arr[0], second;
+for (i = 1; i < n; i++)
+{
+if (arr[i] > largest)
+{
+second = largest;
+largest = arr[i];
+}
+else if (arr[i] > second && arr[i] != largest)
+{
+second = arr[i];
+}
+}
+if (n < 2)
+else
+printf("Second largest doesn't exist.\n");
+printf("Second largest number = %d\n", second);
+return 0;
+}
